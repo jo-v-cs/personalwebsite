d66cd8bfdb249c1a80c95e0ba682ab0cd8fe02cb
jo-v-cs <jvioo@yahoo.com>
2021-07-30 00:56:55 -0400

    Add contact form response

--- contactValidation.js
+++ contactValidation.js
@@ -1,4 +1,10 @@
-const contactForm = document.getElementById('contact-form');
-contactForm.addEventListener("submit", function(e) {
-    console.log(contactForm);    
-})
+const form = document.getElementById('contact-form');
+
+form.addEventListener('submit', function(e) {
+    console.log("Form submitted");
+    let visitorName = document.getElementById('visitorName').value;
+    let visitorEmail = document.getElementById('visitorEmail').value;
+    let visitorPhone = document.getElementById('visitorPhone').value;
+    console.log(visitorName);
+    document.getElementById('userInput').innerHTML = "<p>Thanks for reaching out, " + visitorName + "!</p>";
+});
