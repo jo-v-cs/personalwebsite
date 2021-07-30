fb7aafe12007bceb2ec4fd95d60643fb26455fbe
jo-v-cs <jvioo@yahoo.com>
2021-07-30 01:05:29 -0400

    Add textarea to ontact form

--- contactValidation.js
+++ contactValidation.js
@@ -3,8 +3,10 @@ const form = document.getElementById('contact-form');
 form.addEventListener('submit', function(e) {
     console.log("Form submitted");
     let visitorName = document.getElementById('visitorName').value;
+    /* These values never used!
     let visitorEmail = document.getElementById('visitorEmail').value;
     let visitorPhone = document.getElementById('visitorPhone').value;
+    */
     console.log(visitorName);
     document.getElementById('userInput').innerHTML = "<p>Thanks for reaching out, " + visitorName + "!</p>";
 });
